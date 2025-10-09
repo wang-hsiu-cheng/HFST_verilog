951fec26b2ba426cb47327df3f877268bf3f8b80
TWang <wang.tw.ted@gmail.com>
2025-10-09 22:44:00 +0800

    Refactor encoder and decoder logic in ip and tcp modules; streamline checksum handling and remove unnecessary state variables

--- hdl/network/ip.v
+++ hdl/network/ip.v
@@ -26,36 +26,29 @@ localparam VERSION = 4'd4, HEADER_LEN = 4'd5, TOS = 8'd0, FLAG = 3'd0, OFFSET = 
 reg [2415:0] tx_ip_reg, tx_ip_reg_1;
 reg [2256:0] tx_tcp_reg;
 reg [15:0] length, ldent;
-reg before_checksum;
 wire [15:0] data_checksum;
 
 always@(*) begin
-    if (before_checksum) begin
-        length = PAYLOAD_LEN + TCPH_LEN + HEADER_LEN * 4;
-        tx_ip_reg[3:0] = VERSION;
-        tx_ip_reg[7:4] = HEADER_LEN;
-        tx_ip_reg[15:8] = TOS;
-        tx_ip_reg[31:16] = length;
-        tx_ip_reg[47:32] = ldent;
-        tx_ip_reg[50:48] = FLAG;
-        tx_ip_reg[63:51] = OFFSET;
-        tx_ip_reg[71:64] = TTL;
-        tx_ip_reg[79:72] = PROTOCOL;
-        tx_ip_reg[95:80] = 0;
-        tx_ip_reg[127:96] = SRCADDR;
-        tx_ip_reg[159:128] = DESADDR;
-        tx_ip_reg[2415:160] = rx_tcp_data;
-        next_ldent = ldent + 1;
-        tx_ip_reg_1 = tx_ip_reg;
-    end
-    else begin
-        length = length;
-        tx_ip_reg[2415:0] = tx_ip_reg[2415:0];
-        next_ldent = ldent;
-        tx_ip_reg_1[79:0] = tx_ip_reg_1[79:0];
-        tx_ip_reg_1[95:80] = data_checksum;
-        tx_ip_reg_1[127:160] = tx_ip_reg_1[2415:160];
-    end
+    // encoder
+    tx_ip_reg_1[79:0] = tx_ip_reg[79:0];
+    tx_ip_reg_1[95:80] = data_checksum;
+    tx_ip_reg_1[2415:96] = tx_ip_reg[2415:96];
+
+    length = PAYLOAD_LEN + TCPH_LEN + HEADER_LEN * 4;
+    tx_ip_reg[3:0] = VERSION;
+    tx_ip_reg[7:4] = HEADER_LEN;
+    tx_ip_reg[15:8] = TOS;
+    tx_ip_reg[31:16] = length;
+    tx_ip_reg[47:32] = ldent;
+    tx_ip_reg[50:48] = FLAG;
+    tx_ip_reg[63:51] = OFFSET;
+    tx_ip_reg[71:64] = TTL;
+    tx_ip_reg[79:72] = PROTOCOL;
+    tx_ip_reg[127:96] = SRCADDR;
+    tx_ip_reg[159:128] = DESADDR;
+    tx_ip_reg[2415:160] = rx_tcp_data;
+    next_ldent = ldent + 1;
+    // decoder
     tx_tcp_reg = rx_ip_data[2415:160];
 end
 
@@ -64,13 +57,11 @@ always@(posedge clk) begin
         ldent <= 0;
         tx_ip_data <= 0;
         tx_tcp_data <= 0;
-        before_checksum <= 0;
     end
     else begin
         ldent <= next_ldent;
         tx_ip_data <= tx_ip_reg_1;
         tx_tcp_data <= tx_tcp_reg;
-        before_checksum <= ~before_checksum;
     end
 end
 endmodule
--- hdl/network/tcp.v
+++ hdl/network/tcp.v
@@ -45,80 +45,64 @@ reg [15:0] rcv_dst_port, rcv_src_port;
 
 always@(*) begin
     if (flag == SYN) begin
-        if (before_checksum) begin
-            pseudo_pi_header[31:0] = SRCADDR;
-            pseudo_pi_header[63:32] = DESADDR;
-            pseudo_pi_header[71:64] = 0;
-            pseudo_pi_header[79:72] = PROTOCOL;
-            pseudo_pi_header[95:80] = TCPH_LEN + PAYLOAD_LEN;
-            seq_num = {rand_val, rand_val};
-            ack = 0;
-            if (last_adv_window != 0) begin
-                adv_window = 0;
-            end
-            else begin
-                adv_window = 302;
-            end
-            tx_tcp_reg[15:0] = rand_val + 16'd1024;
-            tx_tcp_reg[31:16] = DSTPORT;
-            tx_tcp_reg[63:32] = seq_num;
-            tx_tcp_reg[95:64] = ack;
-            tx_tcp_reg[99:96] = HDRLEN;
-            tx_tcp_reg[105:100] = 6'd0;
-            tx_tcp_reg[111:106] = flag;
-            tx_tcp_reg[127:112] = adv_window;
-            tx_tcp_reg[143:128] = 0;
-            tx_tcp_reg[159:144] = URGPTR;
-            tx_tcp_reg[(PAYLOAD_LEN + TCPH_LEN)*8-1:160] = rx_fix_data;
-            tx_tcp_reg_1 = tx_tcp_reg;
+        tx_tcp_reg_1[127:0] = tx_tcp_reg[127:0];
+        tx_tcp_reg_1[143:128] = data_checksum;
+        tx_tcp_reg_1[(PAYLOAD_LEN + TCPH_LEN)*8-1:144] = tx_tcp_reg[(PAYLOAD_LEN + TCPH_LEN)*8-1:144];
+
+        pseudo_pi_header[31:0] = SRCADDR;
+        pseudo_pi_header[63:32] = DESADDR;
+        pseudo_pi_header[71:64] = 0;
+        pseudo_pi_header[79:72] = PROTOCOL;
+        pseudo_pi_header[95:80] = TCPH_LEN + PAYLOAD_LEN;
+        seq_num = {rand_val, rand_val};
+        ack = 0;
+        if (last_adv_window != 0) begin
+            adv_window = 0;
         end
         else begin
-            pseudo_pi_header = pseudo_pi_header;
-            seq_num = seq_num;
-            ack = ack;
-            adv_window = last_adv_window;
-            tx_tcp_reg1[127:0] = tx_tcp_reg1[127:0];
-            tx_tcp_reg1[143:128] = data_checksum;
-            tx_tcp_reg1[(PAYLOAD_LEN + TCPH_LEN)*8-1:144] = tx_tcp_reg1[(PAYLOAD_LEN + TCPH_LEN)*8-1:144];
+            adv_window = 302;
         end
+        tx_tcp_reg[15:0] = rand_val + 16'd1024;
+        tx_tcp_reg[31:16] = DSTPORT;
+        tx_tcp_reg[63:32] = seq_num;
+        tx_tcp_reg[95:64] = ack;
+        tx_tcp_reg[99:96] = HDRLEN;
+        tx_tcp_reg[105:100] = 6'd0;
+        tx_tcp_reg[111:106] = flag;
+        tx_tcp_reg[127:112] = adv_window;
+        tx_tcp_reg[143:128] = 0;
+        tx_tcp_reg[159:144] = URGPTR;
+        tx_tcp_reg[(PAYLOAD_LEN + TCPH_LEN)*8-1:160] = rx_fix_data;
     end 
     else begin
-        if (before_checksum) begin
-            pseudo_pi_header[31:0] = SRCADDR;
-            pseudo_pi_header[63:32] = DESADDR;
-            pseudo_pi_header[71:64] = 0;
-            pseudo_pi_header[79:72] = PROTOCOL;
-            pseudo_pi_header[95:80] = TCPH_LEN + PAYLOAD_LEN;
-            seq_num = seq_num + 1;
-            ack = ack + 1;
-            if (last_adv_window != 0) begin
-                adv_window = 0;
-            end
-            else begin
-                adv_window = 302;
-            end
-            tx_tcp_reg[15:0] = rcv_dst_port;
-            tx_tcp_reg[31:16] = rcv_src_port;
-            tx_tcp_reg[63:32] = seq_num;
-            tx_tcp_reg[95:64] = ack;
-            tx_tcp_reg[99:96] = HDRLEN;
-            tx_tcp_reg[105:100] = 6'd0;
-            tx_tcp_reg[111:106] = flag;
-            tx_tcp_reg[127:112] = adv_window;
-            tx_tcp_reg[143:128] = 0;
-            tx_tcp_reg[159:144] = URGPTR;
-            tx_tcp_reg[(PAYLOAD_LEN + TCPH_LEN)*8-1:160] = rx_fix_data;
-            tx_tcp_reg_1 = tx_tcp_reg;
+        tx_tcp_reg_1[127:0] = tx_tcp_reg[127:0];
+        tx_tcp_reg_1[143:128] = data_checksum;
+        tx_tcp_reg_1[(PAYLOAD_LEN + TCPH_LEN)*8-1:144] = tx_tcp_reg[(PAYLOAD_LEN + TCPH_LEN)*8-1:144];
+
+        pseudo_pi_header[31:0] = SRCADDR;
+        pseudo_pi_header[63:32] = DESADDR;
+        pseudo_pi_header[71:64] = 0;
+        pseudo_pi_header[79:72] = PROTOCOL;
+        pseudo_pi_header[95:80] = TCPH_LEN + PAYLOAD_LEN;
+        seq_num = seq_num + 1;
+        ack = ack + 1;
+        if (last_adv_window != 0) begin
+            adv_window = 0;
         end
         else begin
-            pseudo_pi_header = pseudo_pi_header;
-            seq_num = seq_num;
-            ack = ack;
-            adv_window = last_adv_window;
-            tx_tcp_reg1[127:0] = tx_tcp_reg1[127:0];
-            tx_tcp_reg1[143:128] = data_checksum;
-            tx_tcp_reg1[(PAYLOAD_LEN + TCPH_LEN)*8-1:144] = tx_tcp_reg1[(PAYLOAD_LEN + TCPH_LEN)*8-1:144];
+            adv_window = 302;
         end
+        tx_tcp_reg[15:0] = rand_val + 16'd1024;
+        tx_tcp_reg[31:16] = DSTPORT;
+        tx_tcp_reg[63:32] = seq_num;
+        tx_tcp_reg[95:64] = ack;
+        tx_tcp_reg[99:96] = HDRLEN;
+        tx_tcp_reg[105:100] = 6'd0;
+        tx_tcp_reg[111:106] = flag;
+        tx_tcp_reg[127:112] = adv_window;
+        tx_tcp_reg[143:128] = 0;
+        tx_tcp_reg[159:144] = URGPTR;
+        tx_tcp_reg[(PAYLOAD_LEN + TCPH_LEN)*8-1:160] = rx_fix_data;
     end
 end
 
@@ -140,14 +124,12 @@ always@(posedge clk) begin
         tx_tcp_data <= 0;
         tx_fix_data <= 0;
         tx_enable <= 0;
-        before_checksum <= 0;
         last_adv_window <= 302;
     end
     else begin
         tx_tcp_data <= tx_tcp_reg_1;
         tx_fix_data <= tx_fix_reg;
         tx_enable <= tx_enable_reg;
-        before_checksum <= ~before_checksum;
         last_adv_window <= adv_window;
     end
 end
